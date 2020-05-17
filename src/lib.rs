fca34699baeacf90446ce327773a2703ff2db72d
Kevin Del Castillo <quebin31@gmail.com>
2020-05-16 22:49:11 -0500

    Add Rating struct storing scores Impl the manhattan distance for it

--- src/lib.rs
+++ src/lib.rs
@@ -15,4 +15,5 @@
 // You should have received a copy of the GNU General Public License
 // along with recommend.  If not, see <http://www.gnu.org/licenses/>.
 
+pub mod rating;
 pub mod score;
---
+++ src/rating.rs
@@ -0,0 +1,68 @@
+// Copyright (C) 2020 Kevin Del Castillo Ramírez
+//
+// This file is part of recommend.
+//
+// recommend is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// recommend is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with recommend.  If not, see <http://www.gnu.org/licenses/>.
+
+use crate::score::Score;
+use num_traits::real::Real;
+use std::{
+    fmt::Debug,
+    ops::{Add, Sub},
+};
+
+#[derive(Debug, Clone)]
+pub struct Rating<T: Real> {
+    data: Vec<Score<T>>,
+}
+
+impl<'a, T> Rating<T>
+where
+    T: Default + Real + 'a,
+    &'a T: Add<Output = T> + Sub<Output = T>,
+{
+    pub fn manhattan(&'a self, rhs: &'a Self) -> Score<T> {
+        self.data
+            .iter()
+            .zip(&rhs.data)
+            .map(|(l, r)| l - r)
+            .filter_map(|s| {
+                if let Score::Some(s) = s {
+                    let s = s.abs();
+                    Some(Score::Some(s))
+                } else {
+                    None
+                }
+            })
+            .fold(Score::Some(T::zero()), |acc, x| acc + x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn distance() {
+        let r1 = Rating {
+            data: vec![Score::Some(1.), Score::None, Score::Some(1.)],
+        };
+
+        let r2 = Rating {
+            data: vec![Score::Some(1.), Score::Some(1.), Score::Some(2.)],
+        };
+
+        println!("{:?}", r1.manhattan(&r2));
+    }
+}
