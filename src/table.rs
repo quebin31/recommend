53e65da104624a76f0048e43305109f269aae7ce
Kevin Del Castillo <quebin31@gmail.com>
2020-05-20 11:10:40 -0500

    Table that tries to save some space

--- src/table.rs
+++ src/table.rs
@@ -15,51 +15,123 @@
 // You should have received a copy of the GNU General Public License
 // along with recommend.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::record::Record;
-use crate::Distance;
+use crate::{record::Record, Distance};
 use num_traits::real::Real;
 use std::{
+    borrow::Borrow,
+    collections::hash_map::RandomState,
     collections::HashMap,
-    ops::{Add, Sub},
+    hash::{BuildHasher, Hash, Hasher},
+    ops::{AddAssign, Sub},
 };
 
 #[derive(Debug, Clone, Default)]
-pub struct Table<T> {
-    keys: Vec<String>,
-    values: HashMap<String, Record<T>>,
+pub struct Table<I, K, V, S = RandomState>
+where
+    I: Hash + Eq,
+    K: Hash + Eq,
+    S: BuildHasher,
+{
+    hash_builder: S,
+    keys: HashMap<u64, K>,
+    records: HashMap<I, Record<V, S>>,
 }
 
-impl<T: Default> Table<T> {
+pub(crate) fn make_hash<K>(hash_builder: &impl BuildHasher, val: &K) -> u64
+where
+    K: Hash,
+{
+    let mut state = hash_builder.build_hasher();
+    val.hash(&mut state);
+    state.finish()
+}
+
+impl<I, K, V, S> Table<I, K, V, S>
+where
+    I: Hash + Eq + Default,
+    K: Hash + Eq + Default + Clone,
+    V: Default,
+    S: BuildHasher + Default,
+{
     pub fn new() -> Self {
         Default::default()
     }
 
-    pub fn with_keys(keys: &[impl ToString]) -> Self {
+    pub fn with_keys(keys: &[K]) -> Self {
+        let hash_builder = S::default();
+
+        let mut with_hashes = Vec::new();
+        for key in keys {
+            let hash = make_hash(&hash_builder, &key);
+            with_hashes.push((hash, key.clone()));
+        }
+
         Self {
-            keys: keys.iter().map(ToString::to_string).collect(),
+            hash_builder,
+            keys: with_hashes.into_iter().collect(),
             ..Default::default()
         }
     }
-}
 
-impl<T> Table<T> {
-    pub fn insert(&mut self, key: &str, record: Record<T>) {
-        self.values.insert(key.to_string(), record);
+    pub fn hash_key(&self, key: &K) -> u64 {
+        make_hash(&self.hash_builder, key)
     }
 }
 
-impl<'a, T> Table<T>
+impl<I, K, V, S> Table<I, K, V, S>
 where
-    T: Real + 'a,
-    &'a T: Add<Output = T> + Sub<Output = T>,
+    I: Hash + Eq,
+    K: Hash + Eq,
+    S: BuildHasher,
 {
-    pub fn distance_between(&'a self, a: &str, b: &str, method: Distance) -> Option<T> {
-        let s = match method {
-            Distance::Manhattan => self.values[a].manhattan(&self.values[b]),
-            Distance::Euclidean => self.values[a].euclidean(&self.values[b]),
-            Distance::Minkowski(p) => self.values[a].minkowski(&self.values[b], p),
-        };
+    pub fn insert(&mut self, key: I, record: Record<V, S>) {
+        self.records.insert(key, record);
+    }
 
-        s.into_option()
+    pub fn record<Q: ?Sized>(&self, key: &Q) -> Option<&Record<V, S>>
+    where
+        I: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+        self.records.get(key)
+    }
+
+    pub fn record_mut<Q: ?Sized>(&mut self, key: &Q) -> Option<&mut Record<V, S>>
+    where
+        I: Borrow<Q>,
+        Q: Hash + Eq,
+    {
+        self.records.get_mut(key)
+    }
+}
+
+impl<'a, I, K, V, S> Table<I, K, V, S>
+where
+    I: Hash + Eq,
+    K: Hash + Eq,
+    S: BuildHasher,
+    V: Real + AddAssign + 'a,
+    &'a V: Sub<Output = V>,
+{
+    pub fn distance_between<P: ?Sized, Q: ?Sized>(
+        &'a self,
+        a: &P,
+        b: &Q,
+        method: Distance,
+    ) -> Option<V>
+    where
+        I: Borrow<Q>,
+        I: Borrow<P>,
+        P: Hash + Eq,
+        Q: Hash + Eq,
+    {
+        let a = a.borrow();
+        let b = b.borrow();
+
+        match method {
+            Distance::Manhattan => self.record(a)?.manhattan(self.record(b)?),
+            Distance::Euclidean => self.record(a)?.euclidean(self.record(b)?),
+            Distance::Minkowski(p) => self.record(a)?.minkowski(self.record(b)?, p),
+        }
     }
 }
