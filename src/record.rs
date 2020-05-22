5b6cd0f2036f3bf2c871547e44b2211d432762d3
Kevin Del Castillo <quebin31@gmail.com>
2020-05-22 17:58:47 -0500

    Added multiple distances Jaccard index and  distance Cosine similarity Pearson correlation

--- src/record.rs
+++ src/record.rs
@@ -17,11 +17,11 @@
 
 use num_traits::real::Real;
 use std::{
-    collections::{hash_map::RandomState, HashMap},
+    collections::{hash_map::RandomState, HashMap, HashSet},
     default::Default,
     fmt::Debug,
     hash::BuildHasher,
-    ops::{AddAssign, Sub},
+    ops::{AddAssign, MulAssign, Sub},
 };
 
 #[derive(Debug, Clone, Default)]
@@ -64,42 +64,132 @@ where
 impl<'a, V, S> Record<V, S>
 where
     S: BuildHasher,
-    V: Real + AddAssign + 'a,
+    V: Real + AddAssign + MulAssign + 'a,
     &'a V: Sub<Output = V>,
 {
     pub fn manhattan(&'a self, rhs: &'a Self) -> Option<V> {
+        let a_keys: HashSet<_> = self.values.keys().collect();
+        let b_keys: HashSet<_> = rhs.values.keys().collect();
+        let common_keys = a_keys.intersection(&b_keys);
+
         let mut dist = None;
-        for (key, val_a) in &self.values {
-            if let Some(val_b) = rhs.values.get(key) {
-                *dist.get_or_insert(V::zero()) += (val_b - val_a).abs();
-            }
+
+        for key in common_keys {
+            let x = self.values[key];
+            let y = rhs.values[key];
+            *dist.get_or_insert_with(V::zero) += (y - x).abs();
         }
 
         dist
     }
 
     pub fn euclidean(&'a self, rhs: &'a Self) -> Option<V> {
+        let a_keys: HashSet<_> = self.values.keys().collect();
+        let b_keys: HashSet<_> = rhs.values.keys().collect();
+        let common_keys = a_keys.intersection(&b_keys);
+
         let mut dist = None;
-        for (key, val_a) in &self.values {
-            if let Some(val_b) = rhs.values.get(key) {
-                *dist.get_or_insert(V::zero()) += (val_b - val_a).powi(2);
-            }
+
+        for key in common_keys {
+            let x = self.values[key];
+            let y = rhs.values[key];
+            *dist.get_or_insert_with(V::zero) += (y - x).powi(2);
         }
 
         dist.map(V::sqrt)
     }
 
     pub fn minkowski(&'a self, rhs: &'a Self, p: usize) -> Option<V> {
+        let a_keys: HashSet<_> = self.values.keys().collect();
+        let b_keys: HashSet<_> = rhs.values.keys().collect();
+        let common_keys = a_keys.intersection(&b_keys);
+
         let mut dist = None;
-        for (key, val_a) in &self.values {
-            if let Some(val_b) = rhs.values.get(key) {
-                *dist.get_or_insert(V::zero()) += (val_b - val_a).abs().powi(p as i32);
-            }
+
+        for key in common_keys {
+            let x = self.values[key];
+            let y = rhs.values[key];
+            *dist.get_or_insert_with(V::zero) += (y - x).abs().powi(p as i32);
         }
 
         V::from(p)
             .map(|p| dist.map(|v| v.powf(V::one() / p)))
             .flatten()
+    }
+
+    pub fn jaccard_index(&'a self, rhs: &'a Self) -> Option<V> {
+        let lhs_keys: HashSet<_> = self.values.keys().collect();
+        let rhs_keys: HashSet<_> = rhs.values.keys().collect();
+
+        let inter = lhs_keys.intersection(&rhs_keys).count();
+        let union = lhs_keys.union(&rhs_keys).count();
+
+        if union == 0 {
+            None
+        } else {
+            Some(V::from(inter)? / V::from(union)?)
+        }
+    }
+
+    pub fn jaccard_distance(&'a self, rhs: &'a Self) -> Option<V> {
+        Some(V::one() - self.jaccard_index(rhs)?)
+    }
+
+    pub fn cosine_similarity(&'a self, rhs: &'a Self) -> Option<V> {
+        let a_keys: HashSet<_> = self.values.keys().collect();
+        let b_keys: HashSet<_> = rhs.values.keys().collect();
+        let common_keys = a_keys.intersection(&b_keys);
+
+        let mut a_norm = None;
+        let mut b_norm = None;
+        let mut dot_prod = None;
+
+        for key in common_keys {
+            let x = self.values[key];
+            let y = rhs.values[key];
+
+            *a_norm.get_or_insert_with(V::zero) += x.powi(2);
+            *b_norm.get_or_insert_with(V::zero) += y.powi(2);
+            *dot_prod.get_or_insert_with(V::one) *= x * y;
+        }
+
+        let norm = (a_norm? * b_norm?).sqrt();
+
+        Some(dot_prod? / norm)
+    }
+
+    pub fn pearson_correlation(&'a self, rhs: &'a Self) -> Option<V> {
+        let a_keys: HashSet<_> = self.values.keys().collect();
+        let b_keys: HashSet<_> = rhs.values.keys().collect();
+        let common_keys = a_keys.intersection(&b_keys);
+
+        let mut mean_x = None;
+        let mut mean_y = None;
+
+        for key in common_keys.clone() {
+            *mean_x.get_or_insert_with(V::zero) += self.values[key];
+            *mean_y.get_or_insert_with(V::zero) += rhs.values[key];
+        }
+
+        let mean_x = mean_x?;
+        let mean_y = mean_y?;
+
+        let mut cov = None;
+        let mut std_dev_a = None;
+        let mut std_dev_b = None;
+
+        for key in common_keys {
+            let x = self.values[key];
+            let y = rhs.values[key];
+
+            *cov.get_or_insert_with(V::one) *= (x - mean_x) * (y - mean_y);
+            *std_dev_a.get_or_insert_with(V::zero) += (x - mean_x).powi(2);
+            *std_dev_b.get_or_insert_with(V::zero) += (y - mean_y).powi(2);
+        }
+
+        let std_dev = (std_dev_a? * std_dev_b?).sqrt();
+
+        Some(cov? / std_dev)
     }
 }
 
